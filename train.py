087ffad249e15e98f335d0833f058d9d0d4c25d0
Leeon Israel (Leo) <leeoniisrael@gmail.com>
2024-07-17 23:55:54 -0500

    feat: imported train utilities:

---
+++ train.py
@@ -0,0 +1,8 @@
+from torch.utils.data import Dataset
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch
+
+from step_2_dataset import get_train_test_loaders
