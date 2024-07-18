d0933cda8390622ae459e0421aabc1664af18eb2
Leeon Israel (Leo) <leeoniisrael@gmail.com>
2024-07-17 23:56:25 -0500

    feat: define pytorch network

--- train.py
+++ train.py
@@ -6,3 +6,24 @@ import torch.optim as optim
 import torch
 
 from step_2_dataset import get_train_test_loaders
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 6, 3)
+        self.conv3 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 48)
+        self.fc3 = nn.Linear(48, 24)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
